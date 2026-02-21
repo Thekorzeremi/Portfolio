c4614bd6b08971c4ab36b3810b48019bf8bfbf1e
Thekorzeremi <kremi02@outlook.fr>
2026-02-21 01:31:41 +0100

    init; project

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Hero from './components/Hero'
 import Skills from './components/Skills'
 import History from './components/History'
 import Projects from './components/Projects'
-import Contact from './components/Contact'
+// import Contact from './components/Contact'
 import Footer from './components/Footer'
 
 const SECTIONS = [
